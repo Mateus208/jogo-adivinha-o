8f20068e6ced0bde24e64439e6949568ddba980b
Mateus208 <150243292+Mateus208@users.noreply.github.com>
2025-02-21 22:15:15 -0300

    jogo

--- jogo.py
+++ jogo.py
@@ -5,7 +5,7 @@ print("***************")
 numero_secret0 = 42
 
 chute_str = input("Digite o seu número:")
-print("Voçê digitou:", chute_str)
+print("Você digitou:",chute_str)
 chute = int (chute_str)
 
 if( numero_sereto == chute):
